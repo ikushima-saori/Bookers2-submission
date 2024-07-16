9a105240353a0dad7d52df46f482ec1e094cb339
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-16 06:38:32 +0000

    [Add]ユーザー情報編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
 
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:create, :index, :show, :edit, :destroy]
 end
