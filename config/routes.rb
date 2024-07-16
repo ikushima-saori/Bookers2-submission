49c0677d4e2457bdabe70e4b25a1ee1a073facb6
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-16 00:27:31 +0000

    [Add]投稿一覧&[Fix]ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:create, :index, :show, :edit]
   devise_for :users
   root to: 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
+
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:create, :index, :show, :edit]
 end
