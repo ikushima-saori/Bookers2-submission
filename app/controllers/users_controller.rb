8ce1713bf181791ec914fb298aad59e8642c33e6
ikushima-saori <172108271+ikushima-saori@users.noreply.github.com>
2024-07-17 16:39:19 +0000

    [Add]Bootstrap&[fix]バリデーション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,10 +19,13 @@ class UsersController < ApplicationController
   end
 
   def update
-   @user = User.find(params[:id])
-   @user.update(user_params)
-   flash[:notice] = "You have updated user successfully."
-   redirect_to user_path(@user.id)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -34,7 +37,7 @@ class UsersController < ApplicationController
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to user_path(user.id)
+      redirect_to user_path(current_user.id)
     end
   end
 end
